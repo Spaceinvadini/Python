206d147342b860da2f03ff40c221026bfefbde35
TOBY <tobystimpson@lineone.net>
2015-10-29 19:01:21 +0000

    tweeting website

---
+++ twitter/tweeting.py
@@ -0,0 +1,25 @@
+import twitter, datetime
+import urllib2
+
+file = open("keys.txt")
+creds = file.readline().strip().split(',')
+
+currentSession = open("/Users/tobystimpson/Library/Application Support/Google/Chrome/Default/Current Session")
+twit = currentSession.read()
+lines = twit.splitlines()
+
+webaddress = " "
+for line in lines:
+    if (line.find("//") != -1):
+        startIndex = line.rfind("//") + 2
+        endIndex = line.rfind("/")
+        webaddress = line[startIndex:endIndex]
+
+api = twitter.Api(creds[0], creds[1], creds[2], creds[3])
+
+timestamp = datetime.datetime.utcnow()
+
+#response = api.PostUpdate("Tweeted at " + str(timestamp))
+response = api.PostUpdate("I like " + webaddress)
+
+print("Status update to: " + response.text)
